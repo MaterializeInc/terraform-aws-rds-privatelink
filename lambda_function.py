582465c699f9b25b5cfd2c0f436d9a7673a23e59
Bobby Iliev <bobby@bobbyiliev.com>
2023-05-17 16:43:07 +0300

    Add Lambda RDS ip check function (#1)
    * Add Lambda RDS ip check function
    
    * Update lambda_function.py
    
    Co-authored-by: Dennis Hume <dehume@users.noreply.github.com>
    
    * Update lambda_function.py
    
    Co-authored-by: Nikhil Benesch <nikhil.benesch@gmail.com>
    
    * Make the cron time configurable
    
    ---------
    
    Co-authored-by: Dennis Hume <dehume@users.noreply.github.com>
    Co-authored-by: Nikhil Benesch <nikhil.benesch@gmail.com>

---
+++ lambda_function.py
@@ -0,0 +1,57 @@
+import boto3
+import socket
+import os
+
+# Define the clients at the top of your function
+elbv2_client = boto3.client('elbv2')
+rds_client = boto3.client('rds')
+
+RDS_IDENTIFIER = os.environ['RDS_IDENTIFIER']  # RDS instance identifier
+TARGET_GROUP_ARN = os.environ['TARGET_GROUP_ARN']  # Target Group ARN
+
+
+def lambda_handler(event, context):
+    # Retrieve the current IP address of the RDS instance
+    rds_instances = rds_client.describe_db_instances(
+        DBInstanceIdentifier=RDS_IDENTIFIER)
+    rds_endpoint = rds_instances['DBInstances'][0]['Endpoint']['Address']
+    ip_address = socket.gethostbyname(rds_endpoint)
+    rds_port = rds_instances['DBInstances'][0]['Endpoint']['Port']
+
+    # Retrieve the existing target of the target group
+    targets = elbv2_client.describe_target_health(
+        TargetGroupArn=TARGET_GROUP_ARN)
+
+    # Get the current IP address in the target group
+    if targets['TargetHealthDescriptions']:
+        current_ip = targets['TargetHealthDescriptions'][0]['Target']['Id']
+    else:
+        current_ip = None
+
+    # If the IP addresses don't match, update the target group
+    if current_ip and current_ip != ip_address:
+            # Deregister the current target
+            elbv2_client.deregister_targets(
+                TargetGroupArn=TARGET_GROUP_ARN,
+                Targets=[
+                    {
+                        'Id': current_ip
+                    },
+                ]
+            )
+
+        # Register the new target
+        elbv2_client.register_targets(
+            TargetGroupArn=TARGET_GROUP_ARN,
+            Targets=[
+                {
+                    'Id': ip_address,
+                    'Port': rds_port
+                },
+            ]
+        )
+
+    return {
+        'statusCode': 200,
+        'body': f'Target group updated. Current target IP: {ip_address}'
+    }
--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Create a target group for the RDS instance
 resource "aws_lb_target_group" "mz_rds_target_group" {
-  name        = "mz-rds-tg-${substr(var.mz_rds_instance_name, 0, 22)}"
+  name        = "mz-rds-${substr(var.mz_rds_instance_name, 0, 12)}-tg"
   port        = data.aws_db_instance.mz_rds_instance.port
   protocol    = "TCP"
   vpc_id      = data.aws_vpc.mz_rds_vpc.id
@@ -11,11 +11,14 @@ resource "aws_lb_target_group" "mz_rds_target_group" {
 resource "aws_lb_target_group_attachment" "mz_rds_target_group_attachment" {
   target_group_arn = aws_lb_target_group.mz_rds_target_group.arn
   target_id        = data.dns_a_record_set.rds_ip.addrs[0]
+  lifecycle {
+    ignore_changes = [target_id]
+  }
 }
 
 # Create a network Load Balancer
 resource "aws_lb" "mz_rds_lb" {
-  name               = "mz-rds-lb-${var.mz_rds_instance_name}"
+  name               = "mz-rds-${substr(var.mz_rds_instance_name, 0, 12)}-lb"
   internal           = true
   load_balancer_type = "network"
   subnets            = values(data.aws_subnet.mz_rds_subnet)[*].id
@@ -42,4 +45,73 @@ resource "aws_vpc_endpoint_service" "mz_rds_lb_endpoint_service" {
   tags = {
     Name = "mz-rds-lb-endpoint-service"
   }
+}
+
+# Create an IAM policy for the Lambda function
+resource "aws_iam_role" "lambda_execution_role" {
+  name               = "lambda_execution_${substr(var.mz_rds_instance_name, 0, 12)}-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+}
+
+# Create a Lambda function to check the RDS instance IP address
+resource "aws_lambda_function" "check_rds_ip" {
+  function_name = "${substr(var.mz_rds_instance_name, 0, 12)}-check-rds-ip"
+  role          = aws_iam_role.lambda_execution_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+
+  filename = data.archive_file.lambda_zip.output_path
+
+  environment {
+    variables = {
+      RDS_IDENTIFIER   = var.mz_rds_instance_name
+      TARGET_GROUP_ARN = aws_lb_target_group.mz_rds_target_group.arn
+    }
+  }
+}
+
+# Create an IAM policy for the Lambda function
+resource "aws_iam_role_policy" "lambda_execution_role_policy" {
+  name   = "${substr(var.mz_rds_instance_name, 0, 12)}-lambda-execution-role-policy"
+  role   = aws_iam_role.lambda_execution_role.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "rds:DescribeDBInstances",
+        "elasticloadbalancing:DescribeTargetHealth",
+        "elasticloadbalancing:RegisterTargets",
+        "elasticloadbalancing:DeregisterTargets"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_cloudwatch_event_rule" "rds_ip_check_rule" {
+  name                = "${substr(var.mz_rds_instance_name, 0, 12)}-rds-ip-check-rule"
+  description         = "Fires every ${var.schedule_expression} to check the RDS instance IP address"
+  schedule_expression = var.schedule_expression
+}
+
+resource "aws_cloudwatch_event_target" "check_rds_ip_event_target" {
+  rule      = aws_cloudwatch_event_rule.rds_ip_check_rule.name
+  target_id = "${substr(var.mz_rds_instance_name, 0, 12)}-check-rds-ip"
+  arn       = aws_lambda_function.check_rds_ip.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_rds_ip" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.check_rds_ip.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.rds_ip_check_rule.arn
 }
