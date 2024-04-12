9301300283cbe21303a403ceeda6ccab0176a58c
Bobby Iliev <bobby@bobbyiliev.com>
2024-04-12 18:42:28 +0300

    Add logging to the lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,4 @@
+import logging
 import boto3
 import socket
 import os
@@ -7,11 +8,16 @@ import json
 elbv2_client = boto3.client('elbv2')
 rds_client = boto3.client('rds')
 
+# Initialize logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 # Load RDS details from environment variables
 RDS_DETAILS = json.loads(os.environ['RDS_DETAILS'])
 
 def update_target_registration(rds_identifier, details):
     try:
+        logger.info(f"Updating target registration for {rds_identifier}")
         # Retrieve the current IP address of the RDS instance
         rds_instances = rds_client.describe_db_instances(DBInstanceIdentifier=rds_identifier)
         rds_port = rds_instances['DBInstances'][0]['Endpoint']['Port']
@@ -38,11 +44,14 @@ def update_target_registration(rds_identifier, details):
         else:
             message = f"Target group {target_group_arn} already up to date. Current target IP: {ip_address} and Port: {rds_port}"
 
+        logger.info(message)
         return {'success': True, 'message': message}
     except Exception as e:
+        logger.error(f"Error updating target registration for {rds_identifier}: {e}")
         return {'success': False, 'message': f"Failed to update targets for {rds_identifier} with error: {e}"}
 
 def lambda_handler(event, context):
+    logger.info("Handler invoked")
     update_messages = []
     all_success = True
 
@@ -51,8 +60,10 @@ def lambda_handler(event, context):
         update_messages.append(result['message'])
         if not result['success']:
             all_success = False
+            logger.warning(f"Update failed for {rds_identifier}")
 
     status_code = 200 if all_success else 500
+    logger.info(f"Function completed with status code {status_code}")
 
     return {
         'statusCode': status_code,
